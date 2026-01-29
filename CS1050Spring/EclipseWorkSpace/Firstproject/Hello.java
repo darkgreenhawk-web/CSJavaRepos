32e3126a914b92781049ed6650e096a76ffd70b2
darkgreenhawk-web <darkgreenhawk@gmail.com>
2026-01-29 09:13:41 -0700

    Update Hello.java

--- CS1050Spring/EclipseWorkSpace/Firstproject/Hello.java
+++ CS1050Spring/EclipseWorkSpace/Firstproject/Hello.java
@@ -12,7 +12,7 @@ public class Hello {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello java");
+		System.out.println("hello weeee");
 	}
 
 }
