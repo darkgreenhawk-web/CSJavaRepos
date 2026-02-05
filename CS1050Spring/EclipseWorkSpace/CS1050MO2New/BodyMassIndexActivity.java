e9424bdbe72fedfb507a9256eb89f01b3587cd86
darkgreenhawk-web <darkgreenhawk@gmail.com>
2026-02-05 13:36:31 -0700

    BMI code

---
+++ CS1050Spring/EclipseWorkSpace/CS1050MO2New/BodyMassIndexActivity.java
@@ -0,0 +1,47 @@
+/**
+ * Calculate the BMI using formula in US units. 
+ * User will enter weight in pounds and height in inches
+ */
+
+// ADD CODE to import Scanner class
+import java.util.Scanner;
+
+
+public class BodyMassIndexActivity
+{
+
+	public static void main(String[] args)
+	{
+		//Create constants an variables
+		// constants
+		// factor formula in pounds
+		final int BMI_US_FACTOR = 703;
+		// Variables
+		// The user's weight
+		double userWeight = -1;
+		// The user's height
+		double userHeight = -1;
+		// The user's BMI
+		double userBMI;
+		
+		// Create a Scanner object for scanning input from keyboard
+		Scanner keyboardInput = new Scanner(System.in);
+		// Tell the user what the program will do.
+		System.out.println("This program will calculate your " + "body mass index, or BMI.");
+
+		// Get the user's weight.
+		System.out.print("Enter your weight, in pounds: ");
+		// ADD CODE to input next double for weight
+		userWeight = keyboardInput.nextDouble();
+		// Get the user's height.
+		System.out.print("Enter your height, in inches: ");
+		// ADD CODE input next double for height
+		userHeight = keyboardInput.nextDouble();
+		// Calculate the user's body mass index.
+		userBMI = userWeight * BMI_US_FACTOR / (userHeight * userHeight);
+
+		// Display the user's BMI.
+		System.out.printf("Your body mass index (BMI) is %f", userBMI);
+
+	}
+}
--- CS1050Spring/EclipseWorkSpace/CS1050m01/GEM01GradeCalculator.java
+++ CS1050Spring/EclipseWorkSpace/CS1050m01/GEM01GradeCalculator.java
@@ -21,13 +21,13 @@ public class GEM01GradeCalculator {
 		final double QUIZ_W = 0.25;
 		final double PROJECTS_W = 0.2;
 		final double FINAL_DEMO_W = 0.2;
-		//Declaring the variable doubles for the grade of each category.
+		//Declaring the variable doubles for the grade of each category. 
 		double classPartGrade = 95.0;
 		double guidedExpGrade = 92.0;
 		double quizGrade = 88.0;
 		double projectsGrade = 87.0;
 		double finalDemoGrade = 93.0;
-		
+		//Declaring the finalGrade as a double then in
 		double finalGrade = CLASS_PART_W * classPartGrade +
 				GUIDED_EXP_W * guidedExpGrade +
 				QUIZ_W * quizGrade +
