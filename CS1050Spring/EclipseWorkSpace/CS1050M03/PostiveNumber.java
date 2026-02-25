1260e0e3e351a8be85caa87e9df34def4c82d40c
darkgreenhawk-web <darkgreenhawk@gmail.com>
2026-02-24 22:12:24 -0700

    Update PostiveNumber.java

--- CS1050Spring/EclipseWorkSpace/CS1050M03/PostiveNumber.java
+++ CS1050Spring/EclipseWorkSpace/CS1050M03/PostiveNumber.java
@@ -13,15 +13,27 @@ public class PostiveNumber {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner keyboardInput = new Scanner(System.in);
-		int number;
-		System.out.println("Enter a postitive number.");
-		number = keyboardInput.nextInt();
-		while (number <= 0)
+		int exponent;
+		double base;
+		System.out.println("Enter a postitive number for an exponent:");
+		exponent = keyboardInput.nextInt();
+		while (exponent <= 0)
 		{
 			System.out.print("Invalid number! Please enter a vailid number:");
-			number = keyboardInput.nextInt();
+			exponent = keyboardInput.nextInt();
 		}
-		System.out.println("Your number is " + number);
+		System.out.println("Your exponent is " + exponent);
+		
+		
+		System.out.print("Enter a number as a base:");
+		base = keyboardInput.nextDouble();
+		for (int i = 1; i < exponent; i++)
+		{
+			base *= base;
+		}
+		System.out.println("Results of base: " + base);
+			
+		keyboardInput.close();
 	}
 
 }
