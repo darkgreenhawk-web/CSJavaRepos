4bc3f1a265633fce9e71645dd1ed9ca38429b968
darkgreenhawk-web <darkgreenhawk@gmail.com>
2026-03-12 10:43:18 -0600

    Update CalculateSum.java

--- CS1050Spring/EclipseWorkSpace/CS1050M03/CalculateSum.java
+++ CS1050Spring/EclipseWorkSpace/CS1050M03/CalculateSum.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-
+import java.util.Scanner;
 /**
  * 
  */
@@ -12,7 +12,21 @@ public class CalculateSum {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Scanner input = new Scanner(System.in);
+		
+		int max;
+		int sum;
+		max = input.nextInt();
+		sum = calculateSum(max);
+		System.out.println("Sum is " + sum + " Max is " + max);
 	}
-
+	public static int calculateSum(int max) {
+		int sum = 0;
+		for (int i = 1; i <= max; i++) {
+			sum = sum + i;
+		}
+			
+		return sum;
+	}
 }
