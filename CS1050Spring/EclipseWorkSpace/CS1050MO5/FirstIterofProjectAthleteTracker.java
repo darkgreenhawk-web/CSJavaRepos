c790c08d01129ace3c008055d56b68dcf685b869
darkgreenhawk-web <darkgreenhawk@gmail.com>
2026-04-09 20:58:43 -0600

    Update FirstIterofProjectAthleteTracker.java

--- CS1050Spring/EclipseWorkSpace/CS1050MO5/FirstIterofProjectAthleteTracker.java
+++ CS1050Spring/EclipseWorkSpace/CS1050MO5/FirstIterofProjectAthleteTracker.java
@@ -23,7 +23,7 @@ public class FirstIterofProjectAthleteTracker {
 		double[] weight = {180.5, 200.7, 215.5, };
 		
 // make better comments
-//new change made made a new change
+//new change made made a new change asd
 	}
 
 	
